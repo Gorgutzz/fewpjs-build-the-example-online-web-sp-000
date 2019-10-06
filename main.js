4c2977db332bce9ac18010454ce4078c4fddd06b
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-06 22:25:59 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -8,9 +8,14 @@ const likeHearts = document.querySelectorAll('.like-glyph');
     heart.addEventListener('click', () => {
       mimicServerCall()
       .then(resp => {
-        heart.innerText === EMPTY_HEART ? heart.innerText = FULL_HEART : heart.innerText = EMPTY_HEART
-        heart.className === 'activated-heart' ? heart.className = '' : heart.className = 'activated-heart'
-        console.log(resp)
+        let likeHeart = likeButton.children[0];
+        if(likeHeart.innerHTML == EMPTY_HEART){
+          likeHeart.innerHTML = FULL_HEART;
+          likeHeart.className = 'activated-heart';
+        } else {
+          likeHeart.innerHTML = EMPTY_HEART;
+          likeHeart.className = '';
+        }
       })
       .catch(resp => {
         const errorModalDiv = document.getElementById('modal')
