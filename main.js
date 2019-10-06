fad4261ce9e006a3df3caf262415be72ba59f677
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-06 22:18:52 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,26 +3,9 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-const addLikeEvent = () => {
-  const likeHearts = document.querySelectorAll('.like-glyph');
-    likeHearts.forEach((heart) => {
-      heart.addEventListener('click', () => {
-        mimicServerCall()
-        .then(resp => {
-          heart.innerText === EMPTY_HEART ? heart.innerText = FULL_HEART : heart.innerText = EMPTY_HEART
-          heart.className === 'activated-heart' ? heart.className = '' : heart.className = 'activated-heart'
-          console.log(resp)
-        })
-        .catch(resp => {
-          const errorModalDiv = document.getElementById('modal')
-          errorModalDiv.className = ''
-          const errorModal = document.getElementById('modal-message')
-          errorModal.innerText = resp
-          setTimeout(() => {errorModalDiv.className = 'hidden'}, 5000)
-        })
-      })
-    })
-}
+
+
+
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
